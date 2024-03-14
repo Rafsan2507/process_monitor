d0f7c9f0279a7449f24d10314a16ce3c868aa905
Rafsan Khan <khanrafsan25@gmail.com>
2024-03-14 10:30:49 +0600

    created an instance to hold json data and inserted random results in each monitor. Then converted this data to JSON again

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use serde_derive::{Deserialize, Serialize};
 use serde_json;
 use rand::{thread_rng, Rng};
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::fs::File;
+use std::io::Write;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Monitor {
-    monitor_id: Option<u32>,
+    monitor_id: Option<u64>,
     name: String,
     #[serde(rename = "type")]
     mytype: Option<String>,
@@ -20,13 +23,13 @@ struct Monitors {
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Result1{
-    value: i32,
-    processed_at: i32
+    value: u64,
+    processed_at: u64
 }
 
 fn main() -> Result<(), std::io::Error> {
     let input_path = std::env::args().nth(1).unwrap();
-    //let output_path = std::env::args().nth(2).unwrap();
+    
     let monitor1 = {
         let monitor = std::fs::read_to_string(&input_path)?;
 
@@ -34,25 +37,39 @@ fn main() -> Result<(), std::io::Error> {
         serde_json::from_str::<Monitors>(&monitor).unwrap()
     };
 
-    // Print the value of `monitor1`
-    //println!("{:?}", monitor1);
+
+    let now = SystemTime::now();
+
+    let duration_since_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
+
+    let seconds_since_epoch = duration_since_epoch.as_secs();
+
     let mut rng = thread_rng();
     let mut my_instance = Result1 {
         value:0,
         processed_at: 0,
     };
 
+    let mut file = File::create("D:/process_monitor/assets/output.json").expect("Failed to create file");
+    let mut vector: Vec<serde_json::Value> = Vec::new();
+
     for m in monitor1.monitors {
         my_instance.value = rng.gen_range(0..100);
-        my_instance.processed_at = rng.gen_range(0..100);
+        my_instance.processed_at = seconds_since_epoch;
+
         let result_data = Result1 {
             value: my_instance.value,
             processed_at: my_instance.processed_at,
         };
         println!("Monitor: {:?}, Results: {:?}", m, result_data);
+
+        vector.push(serde_json::to_value(m).unwrap());
+        vector.push(serde_json::to_value(result_data).unwrap());
+
     }
+    let json_output = serde_json::to_string_pretty(&vector).unwrap();
+        file.write_all(json_output.as_bytes())
+        .expect("Unable to write data to file");
 
     Ok(())
-
-    
 }
